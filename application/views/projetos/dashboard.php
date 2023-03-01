3099d82e9e27791bc3e9c960ba145e1761cb6b8f
adautoaquino <adautoaquinopegado@gmail.com>
2023-03-01 19:26:01 -0300

    dash

---
+++ application/views/projetos/dashboard.php
@@ -0,0 +1,283 @@
+ <!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projetos-InovaPM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <script src="../../libraries/jquery_mask/dist/jquery.mask.js"></script>
+    <script src="../../libraries/jquery_mask/dist/jquery.mask.min.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+</head>
+
+<body background="<?php echo base_url()?>images/fundo_login.jpeg">
+    <style>
+        .titulo {
+            color: aqua;
+            size: 14px;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+
+        legend {
+            color: black;
+            font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        }
+
+        .loader {
+            position: fixed;
+            left: 0px;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background: url('<?php echo base_url() ?>images/logosemfundo.png') 50% 50% no-repeat;
+            background-size: 30%;
+        }
+
+        .rocket {
+            display: inline-block;
+            margin: 0 0.5rem;
+
+            animation: bounceInUp;
+            animation-duration: 6s;
+
+        }
+    </style>
+
+    <div id="loader" class="loader" style=" background-color: #c10000;">
+        <div class="inner"></div>
+    </div>
+
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="<?php echo base_url() ?>index.php/main/session"><img src="<?php echo base_url() ?>images/logo_inova.png" width="75" height="50"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <li class="nav-item">
+                            <a class="nav-link active" style="color: white" aria-current="page" href="<?php echo base_url() ?>index.php/main/session"><b>Menu</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " style="color: white" href="<?php echo base_url() ?>index.php/projetos/main_pj/gerencia_projetos"><b>Gerenciar Projetos</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " style="color: white" href="<?php echo base_url() ?>index.php/pe/projetos/dashboard"><b>Dashboard de Indicadores</b></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="container text-center" style="background-color: white;">
+        <div class="row" style="padding-top:3%">
+            <div class="col-md-6">
+                <div id="grafico_projetos_finalizados">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div id="grafico_projetos_rodando">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="gerentes_responsaveis">
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+
+
+</html>
+<script>
+    $(window).on('load', function() {
+        $('#loader').fadeIn('slow');
+        $('#loader').delay(1000).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    })
+</script>
+
+<script>
+    // Data retrieved from https://netmarketshare.com
+    Highcharts.chart('grafico_projetos_finalizados', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Projetos Finalizados',
+        align: 'left'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y} projetos</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Projetos Finalizados',
+        colorByPoint: true,
+        data: [{
+            name: 'Finalizados no Prazo',
+            y: <?php echo $dados['finalizado_no_prazo']?>,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Finalizados com Atraso',
+            y: <?php echo $dados['finalizado_atrasado']?>
+        }]
+    }]
+});
+</script>
+<script>
+Highcharts.chart('grafico_projetos_rodando', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Projetos Rodando',
+        align: 'left'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y} projetos</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Projetos Rodando',
+        colorByPoint: true,
+        data: [{
+            name: 'Rodando no Prazo',
+            y: <?php echo $dados_rodando['rodando_no_prazo']?>,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Rodando com Atraso',
+            y: <?php echo $dados_rodando['rodando_atrasado']?>
+        }]
+    }]
+});
+</script>
+
+<script>
+Highcharts.chart('gerentes_responsaveis', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'World\'s largest cities per 2021'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://worldpopulationreview.com/world-cities" target="_blank">World Population Review</a>'
+    },
+    xAxis: {
+        type: 'category',
+        labels: {
+            rotation: -45,
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Population (millions)'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        pointFormat: 'Population in 2021: <b>{point.y:.1f} millions</b>'
+    },
+    series: [{
+        name: 'Population',
+        data: [
+            ['Tokyo', 37.33],
+            ['Delhi', 31.18],
+            ['Shanghai', 27.79],
+            ['Sao Paulo', 22.23],
+            ['Mexico City', 21.91],
+            ['Dhaka', 21.74],
+            ['Cairo', 21.32],
+            ['Beijing', 20.89],
+            ['Mumbai', 20.67],
+            ['Osaka', 19.11],
+            ['Karachi', 16.45],
+            ['Chongqing', 16.38],
+            ['Istanbul', 15.41],
+            ['Buenos Aires', 15.25],
+            ['Kolkata', 14.974],
+            ['Kinshasa', 14.970],
+            ['Lagos', 14.86],
+            ['Manila', 14.16],
+            ['Tianjin', 13.79],
+            ['Guangzhou', 13.64]
+        ],
+        dataLabels: {
+            enabled: true,
+            rotation: -90,
+            color: '#FFFFFF',
+            align: 'right',
+            format: '{point.y:.1f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    }]
+});
+
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
